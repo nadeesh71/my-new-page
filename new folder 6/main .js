de95382bb573e1efd140421b69579d5073b9ff95
nadeesh71 <nadeeshfernando327@gmail.com>
2025-03-13 12:00:19 +0530

    update new

---
+++ new folder 6/main .js
@@ -0,0 +1,7 @@
+// Example JavaScript (you can add your functionality here)
+console.log("Website loaded!");
+
+//Example of adding a class to the body on load.
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.classList.add('loaded');
+});
